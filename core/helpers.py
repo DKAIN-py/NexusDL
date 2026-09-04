0d5abf174b8bbc059ef458264ac1a61ab05e0054
DKAIN-py <divyanshu06kain@gmail.com>
2026-09-04 22:03:04 +0000

    Vectorized im2col for Conv2d, added Adam optimizer and CNN example for MINST dataset

--- core/helpers.py
+++ core/helpers.py
@@ -3,23 +3,42 @@ from core.Nexus import Nexus, np
 def im2col(x: Nexus, kh: int, kw:int, stride: int = 1, padding: int = 0)->Nexus:
     if padding > 0:
         x = x.pad2d(padding)
-
+    
     N,C,H,W = x.dimension
     H_out = (H-kh)//stride + 1
     W_out = (W-kw)//stride + 1
 
-    patches = []
-    for i in range(H_out):
-        for j in range(W_out):
-            h_start, w_start = i*stride, j*stride
-            patch = x[:, :, h_start:h_start+kh, w_start:w_start+kw]
-            patch_flat = patch.reshape((N, C*kh*kw))
-            patches.append(patch_flat)
+    x_val = x.value
+    shape = (N, C, H_out, W_out, kh, kw)
+    strides = (
+        x_val.strides[0],
+        x_val.strides[1],
+        x_val.strides[2]*stride,
+        x_val.strides[3]*stride,
+        x_val.strides[2],
+        x_val.strides[3],
+    )
+    patches = np.lib.stride_tricks.as_strided(x_val, shape=shape, strides=strides)
+    x_col = patches.transpose(1,4,5,0,2,3).reshape((C*kh*kw, N*H_out*W_out))
 
-    stacked = Nexus.concat([p.reshape((1,N,C*kh*kw)) for p in patches], axis=0)
-    x_col = stacked.transpose(2,1,0).reshape((C*kh*kw, N*H_out*W_out))
-    return x_col
+    out = Nexus(x_col)
+    out._children = {x}
 
+    if Nexus._track_graph:
+        def _backward():
+            grad_col = out.grads.reshape(C, kh, kw, N, H_out, W_out)
+            grad_patches = grad_col.transpose(3,0,1,2,4,5)
+
+            dx = np.zeros_like(x_val)
+            for i in range(kh):
+                for j in range(kw):
+                    dx[:,:,i:i+H_out*stride:stride, j:j+W_out*stride:stride] += grad_patches[:,:,i,j,:,:]
+
+            x.grads += x._handle_broadcast(dx, x.dimension)
+
+        out._backward = _backward
+
+    return out
 
 def maxpool2d(x:Nexus, kernel_size: int | tuple, stride: int = None) -> Nexus:
     kh, kw = (kernel_size, kernel_size) if isinstance(kernel_size, int) else kernel_size
--- extensions/Optimizers.py
+++ extensions/Optimizers.py
@@ -33,3 +33,41 @@ class SGD:
         for p in self.parameters:
             grads_value = p.grads.value if isinstance(p.grads, Nexus) else p.grads
             p.value = p.value - self.lr*grads_value
+
+class Adam:
+    def __init__(self, params, lr: float=0.001, beta1: float =0.9, beta2: float = 0.999, eps: float = 1e-8):
+        self.params = params
+        self.lr = lr
+        self.beta1 = beta1
+        self.beta2 = beta2
+        self.eps = eps
+        self.t = 0
+
+        self.m = [np.zeros_like(p.value) for p in self.params]
+        self.v = [np.zeros_like(p.value) for p in self.params]
+
+    def step(self, max_norm: float = 1.0):
+        self.t+=1
+
+        total_norm = np.sqrt(sum(np.sum(p.grads ** 2) for p in self.params if p.grads is not None))
+        clip_coef = max_norm / (total_norm + 1e-6)
+        if clip_coef < 1:
+            for p in self.params:
+                if p.grads is not None:
+                    p.grads *= clip_coef
+
+        for i,p in enumerate(self.params):
+            if p.grads is None:
+                continue
+
+            self.m[i] = self.beta1*self.m[i] + (1-self.beta1)*p.grads
+            self.v[i] = self.beta2*self.v[i] + (1-self.beta2)*(p.grads**2)
+
+            m_hat = self.m[i]/(1- self.beta1**self.t)
+            v_hat = self.v[i]/(1- self.beta2**self.t)
+
+            p.value -= self.lr*m_hat/(np.sqrt(v_hat) + self.eps)
+
+    def zero_grad(self):
+        for p in self.params:
+            p.grads = np.zeros_like(p.value, dtype=np.float32)
